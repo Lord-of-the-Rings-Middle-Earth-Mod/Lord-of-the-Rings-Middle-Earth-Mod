8072d3d2afcc3f86861ac31e0163e8b8a4c19b47
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2024-12-03 09:35:38 +0100

    Changes to make the Recipes work

--- src/main/java/me/anedhel/lotrmiddleearth/LordOfTheRingsMiddleEarthMod.java
+++ src/main/java/me/anedhel/lotrmiddleearth/LordOfTheRingsMiddleEarthMod.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2024
- * Authors/Developers: Anedhel (Moritz Rohleder)
+ * Copyright (c) 2024-2024
+ * Authors/Developers are listed in the CONTRIBUTING.md
  *
- * This program is free software: you can redistribute it and/or modify
+ * lord-of-the-rings-middle-earth is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * lord-of-the-rings-middle-earth is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package me.anedhel.lotrmiddleearth;
 
 import net.fabricmc.api.ModInitializer;
@@ -25,13 +25,13 @@ import org.slf4j.LoggerFactory;
 public class LordOfTheRingsMiddleEarthMod implements ModInitializer {
 
 	public static final String MOD_ID = "lotr";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final String COPYRIGHT = "LotR ME Mod Copyright (C) 2024\n" +
-			"    Authors are found in the CONTRIBUTORS-File.\n" +
-			"    This program comes with ABSOLUTELY NO WARRANTY.\n" +
-			"    This is free software, and you are welcome to redistribute it\n" +
-			"    under certain conditions, described in the LICENSE-File.";
+	public static final String COPYRIGHT =
+			"LotR ME Mod Copyright (C) 2024\n" + "    Authors are found in the CONTRIBUTORS-File.\n"
+			+ "    This program comes with ABSOLUTELY NO WARRANTY.\n"
+			+ "    This is free software, and you are welcome to redistribute it\n"
+			+ "    under certain conditions, described in the LICENSE-File.";
 
 	@Override
 	public void onInitialize() {
--- src/main/java/me/anedhel/lotrmiddleearth/LordOfTheRingsMiddleEarthModClient.java
+++ src/main/java/me/anedhel/lotrmiddleearth/LordOfTheRingsMiddleEarthModClient.java
@@ -1,13 +1,30 @@
+/*
+ * Copyright (c) 2024-2024
+ * Authors/Developers are listed in the CONTRIBUTING.md
+ *
+ * lord-of-the-rings-middle-earth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lord-of-the-rings-middle-earth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.anedhel.lotrmiddleearth;
 
 import net.fabricmc.api.ClientModInitializer;
 
 public class LordOfTheRingsMiddleEarthModClient implements ClientModInitializer {
-    /**
-     * Runs the mod initializer on the client environment.
-     */
-    @Override
-    public void onInitializeClient() {
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient() {
 
-    }
+	}
 }
